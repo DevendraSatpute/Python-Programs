3705bbfe4f0cf889ccda8ea14e941837ff2abf68
root <root@DESKTOP-JVGK58C.localdomain>
2023-05-28 14:11:02 +0530

    added data types

--- exceptional_handling.py
+++ exceptional_handling.py
@@ -9,7 +9,12 @@ name_convension = {
 }
 # PascalCase
 def ExceptionHandlingExample():
-    pass
+    if int2 == 0:
+        print("It will return raise statement, this statement will not executed any code after raise")
+        raise ZeroDivisionError("cannot divide by zero")
+        print("This line will not be excuted")
+    return int1/int2
+
 
 # snake_case
 def exception_handling_example():
@@ -30,19 +35,55 @@ def exception_handling_example():
 
 # camelCase
 def exceptionHandlingExample():
-    pass
-
+    # here we know more about data types in python
+    # mainly we have two types of data
+    # primitive: int, float, bool, str, None
+    try:
+        student_name = input("Please enter a your name: ") # input function will take string type
+        age = int(input("How old you are: ")) # here we need only whole number
+        married = bool(input("Are you married: ")) # here we only except True or False
+        weight = float(input("please Enter your weight: ")) #here we can enter int or float number
+        approval = None
+    except Exception:
+        print(traceback.format_exc())
+    else:
+        print(f"Student Name : {student_name}. \nAge : {age}. \nMarrid Status : {married}.\nWeight : {weight}.\nApproval: {approval}.")
+    finally:
+        print("Thanks for showing interest..")
 
 if __name__ == '__main__':
+    # we are use argparse module for command line argument
     parser = argparse.ArgumentParser()
     parser.add_argument('-mode', type=str, default='1', help='select name convension functions')
     args = parser.parse_args()
-    if args.mode == '1':
-        print("You are selected {} naming convensions", name_convension.get(args.mode, ''))
-    elif args.mode == '2':
-        import pdb;pdb.set_trace()
-        print("You are selected {} naming convensions",name_convension.get(args.mode,''))
-        exception_handling_example()
-    elif args.mode =='3':
-        print("You are selected {} naming convensions", name_convension.get(args.mode, ''))
 
+    # Here we are defining cases and get method of dictionary
+    if args.mode == '1':
+        int1 = int(input("Please enter a number: "))
+        int2 = int(input("Please another number: "))
+        print("You are selected {} naming conventions".format(name_convension.get(args.mode, '')))
+        try:
+            # here are define variable inside main function it has global scope
+            # Pascal case name conventions used to define the function name
+            result = ExceptionHandlingExample()
+        except ZeroDivisionError as e:
+            print(str(e))
+        except Exception as e:
+            print(traceback.format_exc())
+        else:
+            print("Division of two number is : ", round(result))
+        finally:
+            print("Thanks for using the calculator")
+
+    elif args.mode == '2':
+        print("You are selected {} naming convensions".format(name_convension.get(args.mode,'')))
+        # Snake case name conventions used to define the function name
+        exception_handling_example()
+
+    elif args.mode == '3':
+        print("You are selected {} naming convensions".format(name_convension.get(args.mode, '')))
+        # Camel case name conventions used to define the function name
+        exceptionHandlingExample()
+
+    else:
+        print("Please enter valid mode")
